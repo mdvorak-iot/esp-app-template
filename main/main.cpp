d5b95807d9be6d7b160f26b18b0f0ec9bbf40af9
Michal Dvorak <michal@mdvorak.org>
2021-02-19 14:27:20 +0100

    added aws_iot_shadow component

--- main/main.cpp
+++ main/main.cpp
@@ -1,9 +1,9 @@
+#include <aws_shadow.h>
 #include <double_reset.h>
-#include <driver/gpio.h>
 #include <esp_log.h>
 #include <esp_ota_ops.h>
-#include <esp_sleep.h>
 #include <esp_wifi.h>
+#include <mqtt_client.h>
 #include <nvs_flash.h>
 #include <status_led.h>
 #include <wifi_reconnect.h>
@@ -12,6 +12,24 @@
 static const char TAG[] = "main";
 
 static bool reconfigure = false;
+static bool mqtt_started = true;
+static esp_mqtt_client_handle_t mqtt_client = nullptr;
+static aws_shadow_handle_t shadow_client = nullptr;
+
+static void do_mqtt_connect()
+{
+    if (!mqtt_started)
+    {
+        // Initial connection
+        ESP_ERROR_CHECK_WITHOUT_ABORT(esp_mqtt_client_start(mqtt_client));
+        mqtt_started = true;
+    }
+    else
+    {
+        // Ignore error here
+        esp_mqtt_client_reconnect(mqtt_client);
+    }
+}
 
 static void setup_init()
 {
@@ -37,13 +55,35 @@ static void setup_init()
 
     // Events
     esp_event_handler_register(
-        WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, [](void *, esp_event_base_t, int32_t, void *) { status_led_set_interval(STATUS_LED_DEFAULT, 500, true); }, NULL);
+        WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, [](void *, esp_event_base_t, int32_t, void *) { status_led_set_interval(STATUS_LED_DEFAULT, 500, true); }, nullptr);
     esp_event_handler_register(
-        WIFI_EVENT, WIFI_EVENT_STA_WPS_ER_SUCCESS, [](void *, esp_event_base_t, int32_t, void *) { status_led_set_interval(STATUS_LED_DEFAULT, 500, true); }, NULL);
+        WIFI_EVENT, WIFI_EVENT_STA_WPS_ER_SUCCESS, [](void *, esp_event_base_t, int32_t, void *) { status_led_set_interval(STATUS_LED_DEFAULT, 500, true); }, nullptr);
     esp_event_handler_register(
-        WPS_CONFIG, WPS_CONFIG_EVENT_START, [](void *, esp_event_base_t, int32_t, void *) { status_led_set_interval(STATUS_LED_DEFAULT, 100, true); }, NULL);
+        WPS_CONFIG, WPS_CONFIG_EVENT_START, [](void *, esp_event_base_t, int32_t, void *) { status_led_set_interval(STATUS_LED_DEFAULT, 100, true); }, nullptr);
     esp_event_handler_register(
-        IP_EVENT, IP_EVENT_STA_GOT_IP, [](void *, esp_event_base_t, int32_t, void *) { status_led_set_interval_for(STATUS_LED_DEFAULT, 200, false, 700, true); }, NULL);
+        IP_EVENT, IP_EVENT_STA_GOT_IP, [](void *, esp_event_base_t, int32_t, void *) { status_led_set_interval_for(STATUS_LED_DEFAULT, 200, false, 700, true); }, nullptr);
+
+    // MQTT
+    //    esp_mqtt_client_config_t mqtt_cfg = {};
+    //    mqtt_cfg.host = CONFIG_AWS_IOT_MQTT_HOST;
+    //    mqtt_cfg.port = CONFIG_AWS_IOT_MQTT_PORT;
+    //    mqtt_cfg.client_id = CONFIG_AWS_IOT_MQTT_CLIENT_ID;
+    //    mqtt_cfg.transport = MQTT_TRANSPORT_OVER_SSL;
+    //    mqtt_cfg.protocol_ver = MQTT_PROTOCOL_V_3_1_1;
+    //    mqtt_cfg.cert_pem = (const char *)aws_root_ca_pem_start;
+    //    mqtt_cfg.client_cert_pem = (const char *)certificate_pem_crt_start;
+    //    mqtt_cfg.client_key_pem = (const char *)private_pem_key_start;
+    //
+    //    mqtt_client = esp_mqtt_client_init(&mqtt_cfg);
+    //    ESP_ERROR_CHECK(esp_mqtt_client_register_event(mqtt_client, MQTT_EVENT_ANY, mqtt_event_handler, NULL));
+    //
+    //    esp_event_handler_instance_register(
+    //        IP_EVENT, IP_EVENT_STA_GOT_IP, [](void *, esp_event_base_t, int32_t, void *) { do_mqtt_connect(); },
+    //        nullptr, nullptr);
+    //
+    //    // Shadow
+    //    ESP_ERROR_CHECK(aws_shadow_init(mqtt_client, CONFIG_AWS_IOT_THING_NAME, NULL, &shadow_client));
+    //    ESP_ERROR_CHECK(aws_shadow_handler_register(shadow_client, AWS_SHADOW_EVENT_ANY, shadow_event_handler, NULL));
 }
 
 static void setup_devices()
